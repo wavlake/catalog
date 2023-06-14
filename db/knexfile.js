4bb6fa12d49e71c121adf74d705f2ac4afdca1b6
Michael Rhee <michaelrhee@pm.me>
2023-06-14 09:13:50 -0500

    Add prod config to knexfile

--- db/knexfile.js
+++ db/knexfile.js
@@ -36,4 +36,21 @@ module.exports = {
       tableName: "knex_migrations",
     },
   },
+  prod: {
+    client: "pg",
+    connection: {
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+    },
+    pool: {
+      min: 0,
+      max: 5,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
