76b69dc86026db7e50fa55bd6bf65c40eb29011a
Josh Remaley <joshremaley@gmail.com>
2023-09-01 14:30:34 -0500

    fix migration

--- db/migrations/20230831195539_make_split_content_unique.js
+++ db/migrations/20230831195539_make_split_content_unique.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("split", function (table) {
-    table.uuid("content_id").notNullable().unique().alter();
+    table.uuid("content_id").notNullable().unique();
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.alterTable("split", function (table) {
-    table.uuid("content_id").notNullable().index("idx_split_content_id");
+    table.dropUnique("content_id");
   });
 };
