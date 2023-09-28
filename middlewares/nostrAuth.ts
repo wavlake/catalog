55c24af2b7bbbe8214afeedbe07dc918ac8d0ca5
Josh Remaley <joshremaley@gmail.com>
2023-09-28 13:53:29 -0500

    add nostrAuth middleware

--- middlewares/nostrAuth.ts
+++ middlewares/nostrAuth.ts
@@ -1,5 +1,20 @@
 import asyncHandler from "express-async-handler";
+import { validateEvent, verifySignature } from "nostr-tools";
 
 export const isNostrAuthorized = asyncHandler(async (req, res, next) => {
+  const { nostr: event } = req.body;
+
+  if (!event) {
+    res.status(400).json({ success: false, error: "No event provided" });
+    return;
+  }
+
+  if (!validateEvent(event)) {
+    res.status(400).json({ success: false, error: "Invalid event" });
+  }
+  if (!verifySignature(event)) {
+    res.status(401).json({ success: false, error: "Invalid event signature" });
+  }
+
   next();
 });
