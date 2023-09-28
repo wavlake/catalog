2ae90bd4790e82754eab0ed2b5b14f38495ced5c
Josh Remaley <joshremaley@gmail.com>
2023-09-28 13:53:59 -0500

    udpate routes

--- routes/library.ts
+++ routes/library.ts
@@ -8,6 +8,8 @@ const router = express.Router();
 //////// ROUTES ////////
 
 router.get("/:id", isNostrAuthorized, libraryController.get_user_library);
+router.post("/:id", isNostrAuthorized, libraryController.add_to_library);
+router.delete("/:id", isNostrAuthorized, libraryController.remove_from_library);
 
 // Export router
 export default router;
