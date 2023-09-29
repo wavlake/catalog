4f01fc87cf6e85120f5cb0305df448ec608280bf
Josh Remaley <joshremaley@gmail.com>
2023-09-29 14:37:26 -0500

    udpate nostr auth middleware to use nip98

--- middlewares/nostrAuth.ts
+++ middlewares/nostrAuth.ts
@@ -1,23 +1,91 @@
 import asyncHandler from "express-async-handler";
-import { Event, validateEvent, verifySignature } from "nostr-tools";
+import { Event, Kind, nip98, verifySignature } from "nostr-tools";
+import { formatError } from "../library/errors";
+import { base64 } from "@scure/base";
+
+// TODO - replace validateEvent with the nostr-tools version once the payload hash feature of NIP-98 is implemented
+// https://github.com/nostr-protocol/nips/blob/master/98.md#nostr-event
+// https://github.com/nbd-wtf/nostr-tools/blob/master/nip98.ts
+// https://github.com/nbd-wtf/nostr-tools/issues/307
+const utf8Encoder = new TextEncoder();
+function hashPayload(payload: any): string {
+  return base64.encode(utf8Encoder.encode(JSON.stringify(payload)));
+}
+
+async function validateEvent(
+  event: Event,
+  url: string,
+  method: string,
+  body?: any
+): Promise<boolean> {
+  if (!event) {
+    throw new Error("Invalid nostr event");
+  }
+  if (!verifySignature(event)) {
+    throw new Error("Invalid nostr event, signature invalid");
+  }
+  if (event.kind !== Kind.HttpAuth) {
+    throw new Error("Invalid nostr event, kind invalid");
+  }
+
+  if (!event.created_at) {
+    throw new Error("Invalid nostr event, created_at invalid");
+  }
+
+  // Event must be less than 60 seconds old
+  if (Math.round(new Date().getTime() / 1000) - event.created_at > 60) {
+    throw new Error("Invalid nostr event, expired");
+  }
+
+  const urlTag = event.tags.find((t) => t[0] === "u");
+  if (urlTag?.length !== 1 && urlTag?.[1] !== url) {
+    throw new Error("Invalid nostr event, url tag invalid");
+  }
+
+  const methodTag = event.tags.find((t) => t[0] === "method");
+  if (
+    methodTag?.length !== 1 &&
+    methodTag?.[1].toLowerCase() !== method.toLowerCase()
+  ) {
+    throw new Error("Invalid nostr event, method tag invalid");
+  }
+
+  if (method.toLowerCase() !== "get") {
+    const payloadTag = event.tags.find((t) => t[0] === "payload");
+    if (payloadTag?.length !== 1) {
+      throw new Error("Invalid nostr event, payload tag invalid");
+    }
+    if (payloadTag?.[1] !== hashPayload(body)) {
+      throw new Error("Invalid nostr event, payload tag invalid");
+    }
+  }
+  return true;
+}
 
 export const isNostrAuthorized = asyncHandler(async (req, res, next) => {
-  const { nostrEvent } = req.query;
-  const parsedEvent = JSON.parse(nostrEvent as string) as Event;
+  try {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      const error = formatError(401, "Missing authorization header");
+      next(error);
+      return;
+    }
 
-  if (!parsedEvent) {
-    res.status(400).json({ success: false, error: "No event provided" });
-    return;
-  }
+    const nostrEvent = await nip98.unpackEventFromToken(authorization);
 
-  if (!validateEvent(parsedEvent)) {
-    res.status(400).json({ success: false, error: "Invalid event" });
-  }
-  if (!verifySignature(parsedEvent)) {
-    res.status(401).json({ success: false, error: "Invalid event signature" });
-  }
+    // TODO- replace with nip98.validateEvent, imported from nostr-tools
+    if (!validateEvent(nostrEvent, req.url, req.method, req.body)) {
+      const error = formatError(401, "Invalid event");
+      next(error);
+      return;
+    }
 
-  // https://expressjs.com/en/api.html#res.locals
-  res.locals.parsedEvent = parsedEvent;
-  next();
+    // successfull auth'd, add the event to res.locals so other middleware can use it
+    // https://expressjs.com/en/api.html#res.locals
+    res.locals.authEvent = nostrEvent;
+    next();
+  } catch (err) {
+    const error = formatError(500, err);
+    next(error);
+  }
 });
