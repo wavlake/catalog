8b4bfb7c7629a371a444d23226f898c7b2d2af23
Josh Remaley <joshremaley@gmail.com>
2023-10-02 13:00:09 -0500

    migrate text to uuid

---
+++ db/migrations/20231002173704_update_library_drop_content_id.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("library", function (table) {
+    table.dropColumn("content_id");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("library", function (table) {
+    table.text("content_id").notNullable();
+  });
+};
