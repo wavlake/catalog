a70c699e53bdc9b6de0f259a4cd314ef60012c12
Josh Remaley <joshremaley@gmail.com>
2023-10-02 12:33:06 -0500

    udpate controllers

--- controllers/library.ts
+++ controllers/library.ts
@@ -2,23 +2,61 @@ import asyncHandler from "express-async-handler";
 import { Event } from "nostr-tools";
 import prisma from "../prisma/client";
 import { formatError } from "../library/errors";
+import db from "../library/db";
 
-const get_user_library = asyncHandler(async (req, res, next) => {
-  try {
-    const { pubkey } = res.locals.authEvent as Event;
+const get_user_library = ({
+  artists = false,
+  albums = false,
+  tracks = false,
+}: {
+  artists?: boolean;
+  albums?: boolean;
+  tracks?: boolean;
+}) =>
+  asyncHandler(async (req, res, next) => {
+    try {
+      const { pubkey = "123" } = res.locals.authEvent as Event;
 
-    const libraryContent = await prisma.library.findMany({
-      where: {
-        user_id: pubkey,
-      },
-    });
+      const libraryArtists = artists
+        ? await db
+            .knex("library")
+            .join("artists", "library.content_id", "artists.id")
+            .where({
+              user_id: pubkey,
+            })
+        : [];
 
-    res.json({ success: true, data: libraryContent });
-  } catch (err) {
-    const error = formatError(500, err);
-    next(error);
-  }
-});
+      const libraryAlbums = albums
+        ? await db
+            .knex("library")
+            .join("albums", "library.content_id", "albums.id")
+            .where({
+              user_id: pubkey,
+            })
+        : [];
+
+      const libraryTracks = tracks
+        ? await db
+            .knex("library")
+            .join("tracks", "library.content_id", "tracks.id")
+            .where({
+              user_id: pubkey,
+            })
+        : [];
+
+      res.json({
+        success: true,
+        data: {
+          ...(artists ? { artists: libraryArtists } : {}),
+          ...(albums ? { albums: libraryAlbums } : {}),
+          ...(tracks ? { tracks: libraryTracks } : {}),
+        },
+      });
+    } catch (err) {
+      const error = formatError(500, err);
+      next(error);
+    }
+  });
 
 const add_to_library = asyncHandler(async (req, res, next) => {
   try {
