1e27cf1cb41e81fa646fc929aa350fe561133ce1
Josh Remaley <joshremaley@gmail.com>
2023-10-02 12:32:38 -0500

    update routes

--- routes/library.ts
+++ routes/library.ts
@@ -7,7 +7,21 @@ const router = express.Router();
 
 //////// ROUTES ////////
 
-router.get("/", isNostrAuthorized, libraryController.get_user_library);
+router.get(
+  "/artists",
+  isNostrAuthorized,
+  libraryController.get_user_library({ artists: true })
+);
+router.get(
+  "/albums",
+  isNostrAuthorized,
+  libraryController.get_user_library({ albums: true })
+);
+router.get(
+  "/tracks",
+  isNostrAuthorized,
+  libraryController.get_user_library({ tracks: true })
+);
 router.post("/", isNostrAuthorized, libraryController.add_to_library);
 router.delete("/", isNostrAuthorized, libraryController.remove_from_library);
 
