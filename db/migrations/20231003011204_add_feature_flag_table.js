80b3de98717056d63f597e157b81da385a82f209
Josh Remaley <joshremaley@gmail.com>
2023-10-02 20:33:28 -0500

    add migration

---
+++ db/migrations/20231003011204_add_feature_flag_table.js
@@ -0,0 +1,22 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("feature_flag", function (table) {
+      table.uuid("id").primary().unique();
+      table.string("name").notNullable();
+    })
+    .createTable("user_flag", function (table) {
+      table.string("user_id").notNullable().index("idx_user_flag_id");
+      table.foreign("user_id").references("user.id");
+      table
+        .uuid("feature_flag_id")
+        .notNullable()
+        .index("idx_user_feature_flag_id");
+      table.foreign("feature_flag_id").references("feature_flag.id");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("user_flag")
+    .dropTableIfExists("feature_flag");
+};
