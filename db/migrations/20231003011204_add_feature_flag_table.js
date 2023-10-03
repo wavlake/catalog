e41a33072e3cb7af818c0a41027119ca32ebed09
Josh Remaley <joshremaley@gmail.com>
2023-10-02 20:57:31 -0500

    update db and prisma

--- db/migrations/20231003011204_add_feature_flag_table.js
+++ db/migrations/20231003011204_add_feature_flag_table.js
@@ -1,14 +1,15 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable("feature_flag", function (table) {
-      table.uuid("id").primary().unique();
+      table.increments("id").primary().unique();
       table.string("name").notNullable();
     })
     .createTable("user_flag", function (table) {
+      table.increments("id").primary().unique();
       table.string("user_id").notNullable().index("idx_user_flag_id");
       table.foreign("user_id").references("user.id");
       table
-        .uuid("feature_flag_id")
+        .integer("feature_flag_id")
         .notNullable()
         .index("idx_user_feature_flag_id");
       table.foreign("feature_flag_id").references("feature_flag.id");
