4e88fd00f60b5ea6b5a075669b3ad75ed211c9c4
Josh Remaley <joshremaley@gmail.com>
2023-10-02 20:33:36 -0500

    correct library down migration

--- db/migrations/20231002173704_update_library_content_id_type.js
+++ db/migrations/20231002173704_update_library_content_id_type.js
@@ -14,6 +14,6 @@ exports.up = function (knex) {
  */
 exports.down = function (knex) {
   return knex.schema.alterTable("library", function (table) {
-    table.text("content_id").notNullable();
+    table.text("content_id").notNullable().alter();
   });
 };
