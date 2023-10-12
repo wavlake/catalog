6c1b66932a61452c5788147bc8dfa4d4942b5363
Josh Remaley <joshremaley@gmail.com>
2023-10-12 10:34:26 -0700

    update table schema

--- db/migrations/20231012154828_add_ext_rss_feeds.js
+++ db/migrations/20231012154828_add_ext_rss_feeds.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("external_feed", function (table) {
     table.uuid("guid").primary().unique();
-    table.text("feed_url").notNullable();
+    table.text("title").notNullable();
   });
 };
 
