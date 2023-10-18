5b3f42253671dca71ea8d4bfd0ff3f41d9bf13d9
Josh Remaley <joshremaley@gmail.com>
2023-10-18 09:18:57 -0700

    update sanitize funciton to handle undefined

--- library/htmlSanitization.ts
+++ library/htmlSanitization.ts
@@ -1,6 +1,8 @@
 import sanitizeHtml from "sanitize-html";
 
-export const sanitize = (dirty: string): string => {
+export const sanitize = (dirty?: string): string | undefined => {
+  if (!dirty) return undefined;
+
   const clean = sanitizeHtml(dirty, {
     allowedTags: ["b", "i", "a", "h1", "h2", "ul", "ol", "li", "p", "br"],
     allowedAttributes: {
