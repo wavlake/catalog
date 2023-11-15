7ea144c8eab40483ccd4bff96fdb6b1d69bb79fd
Josh Remaley <joshremaley@gmail.com>
2023-11-15 14:36:38 -0600

    udpate budget migration to include check

--- db/migrations/20231115220722_add_nwc_budget.js
+++ db/migrations/20231115220722_add_nwc_budget.js
@@ -1,11 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("wallet_connection", function (table) {
-    table.integer("budget");
+    table.integer("budget").notNullable();
+    table.check("budget >= 0", [], "connection_budget_check");
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.alterTable("wallet_connection", function (table) {
+    table.dropChecks(["connection_budget_check"]);
     table.dropColumn("budget");
   });
 };
