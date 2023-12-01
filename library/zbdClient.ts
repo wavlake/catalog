d3942106534724e60a88dfd83e4b7917a0023239
Michael Rhee <michaelrhee@pm.me>
2023-12-01 15:54:14 -0600

    Update zbd client

--- library/zbdClient.ts
+++ library/zbdClient.ts
@@ -1,9 +1,15 @@
-import { sendPaymentPayload, sendPaymentResponse } from "../types/zbd";
+import {
+  SendKeysendRequest,
+  SendKeysendResponse,
+  SendPaymentRequest,
+  SendPaymentResponse,
+} from "../types/zbd";
 import log from "loglevel";
 const axios = require("axios").default;
 
 // Create ZBD instance
 const zbdApiKey = process.env.ZBD_API_KEY;
+const zbdCallbackUrl = `${process.env.ZBD_CALLBACK_URL}/payments/callback/zbd`;
 
 export async function getProductionIps(): Promise<Array<string>> {
   const { data } = await axios
@@ -31,13 +37,31 @@ export async function isSupportedRegion(ipAddress: string): Promise<boolean> {
   return data.data.isSupported;
 }
 
+export async function sendKeysend(
+  request: SendKeysendRequest
+): Promise<SendKeysendResponse> {
+  const { data } = await axios
+    .post(
+      `https://api.zebedee.io/v0/keysend-payment`,
+      { callbackUrl: zbdCallbackUrl, ...request },
+      {
+        headers: { apikey: zbdApiKey },
+      }
+    )
+    .catch((err) => {
+      log.trace(err);
+      return err.response;
+    });
+  return data;
+}
+
 export async function sendPayment(
-  payload: sendPaymentPayload
-): Promise<sendPaymentResponse> {
+  request: SendPaymentRequest
+): Promise<SendPaymentResponse> {
   const { data } = await axios
     .post(
       `https://api.zebedee.io/v0/payments`,
-      { ...payload },
+      { callbackUrl: zbdCallbackUrl, ...request },
       {
         headers: { apikey: zbdApiKey },
       }
