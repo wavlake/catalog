de24282c1df91bef3c8c4674ae26831f451ed6a1
Michael Rhee <michaelrhee@pm.me>
2023-12-06 21:18:35 -0600

    Add optional query params

--- controllers/charts.ts
+++ controllers/charts.ts
@@ -5,17 +5,40 @@ const asyncHandler = require("express-async-handler");
 
 // Top 40
 const get_top_forty = asyncHandler(async (req, res, next) => {
-  const request = {
-    limit: req.query.limit ? req.query.limit : 41,
+  const { limit, phase }: { limit: number; phase: string } = {
+    limit: req.query.limit ? parseInt(req.query.limit) : 50,
+    phase: req.query.phase ? req.query.phase : "week", // week by default
   };
 
-  const tracks = await prisma.trackInfo.findMany({
-    where: { msatTotal30Days: { gt: 0 } },
-    orderBy: { msatTotal30Days: "desc" },
-    take: request.limit,
-  });
+  phase === "day" || phase === "week" || phase === "month"
+    ? null
+    : res.json({
+        success: false,
+        error: "Invalid phase, must be one of: day, week, month",
+      });
 
-  res.json({ success: true, data: tracks });
+  if (phase === "day") {
+    const tracks = await prisma.trackInfo.findMany({
+      where: { msatTotal1Days: { gt: 0 } },
+      orderBy: { msatTotal1Days: "desc" },
+      take: limit,
+    });
+    res.json({ success: true, data: tracks });
+  } else if (phase === "month") {
+    const tracks = await prisma.trackInfo.findMany({
+      where: { msatTotal30Days: { gt: 0 } },
+      orderBy: { msatTotal30Days: "desc" },
+      take: limit,
+    });
+    res.json({ success: true, data: tracks });
+  } else if (phase === "week") {
+    const tracks = await prisma.trackInfo.findMany({
+      where: { msatTotal7Days: { gt: 0 } },
+      orderBy: { msatTotal7Days: "desc" },
+      take: limit,
+    });
+    res.json({ success: true, data: tracks });
+  }
 });
 
 export default {
