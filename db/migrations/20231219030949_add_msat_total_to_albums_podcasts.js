8e7b918e8b01aeee76f95f1ddd20d917e2e27822
Josh Remaley <joshremaley@gmail.com>
2023-12-19 16:40:17 -0600

    add negative check to msat total

--- db/migrations/20231219030949_add_msat_total_to_albums_podcasts.js
+++ db/migrations/20231219030949_add_msat_total_to_albums_podcasts.js
@@ -1,19 +1,23 @@
 exports.up = function (knex) {
   return knex.schema
     .alterTable("podcast", function (table) {
-      table.integer("msat_total").defaultTo(0);
+      table.bigInteger("msat_total").notNullable().defaultTo(0);
+      table.check("msat_total >= 0", [], "msat_total_check");
     })
     .alterTable("album", function (table) {
-      table.integer("msat_total").defaultTo(0);
+      table.bigInteger("msat_total").notNullable().defaultTo(0);
+      table.check("msat_total >= 0", [], "msat_total_check");
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
     .alterTable("podcast", function (table) {
+      table.dropChecks(["msat_total_check"]);
       table.dropColumn("msat_total");
     })
     .alterTable("album", function (table) {
+      table.dropChecks(["msat_total_check"]);
       table.dropColumn("msat_total");
     });
 };
