7ce6f4729e629be4c8f675f33564d5075eb12d75
Josh Remaley <joshremaley@gmail.com>
2023-12-20 22:41:44 -0600

    add timestamp to zap_request table

--- db/migrations/20231219030949_add_msat_total_to_albums_podcasts.js
+++ db/migrations/20231219030949_add_msat_total_to_albums_podcasts.js
@@ -7,6 +7,10 @@ exports.up = function (knex) {
     .alterTable("album", function (table) {
       table.bigInteger("msat_total").notNullable().defaultTo(0);
       table.check("msat_total >= 0", [], "album_msat_total_check");
+    })
+    .alterTable("zap_request", function (table) {
+      table.bigInteger("timestamp");
+      table.check("timestamp >= 0", [], "zap_request_timestamp_check");
     });
 };
 
@@ -20,4 +24,8 @@ exports.down = function (knex) {
       table.dropChecks(["album_msat_total_check"]);
       table.dropColumn("msat_total");
     });
+  .alterTable("zap_request", function (table) {
+    table.dropChecks(["zap_request_timestamp_check"]);
+    table.dropColumn("timestamp");
+  });
 };
