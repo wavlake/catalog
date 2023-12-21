46c8fa24943797b880db0e77cd4847f66901124f
Josh Remaley <joshremaley@gmail.com>
2023-12-21 11:23:51 -0600

    revert timestamp change

--- db/migrations/20231219030949_add_msat_total_to_albums_podcasts.js
+++ db/migrations/20231219030949_add_msat_total_to_albums_podcasts.js
@@ -7,10 +7,6 @@ exports.up = function (knex) {
     .alterTable("album", function (table) {
       table.bigInteger("msat_total").notNullable().defaultTo(0);
       table.check("msat_total >= 0", [], "album_msat_total_check");
-    })
-    .alterTable("zap_request", function (table) {
-      table.bigInteger("timestamp");
-      table.check("timestamp >= 0", [], "zap_request_timestamp_check");
     });
 };
 
@@ -24,8 +20,4 @@ exports.down = function (knex) {
       table.dropChecks(["album_msat_total_check"]);
       table.dropColumn("msat_total");
     });
-  .alterTable("zap_request", function (table) {
-    table.dropChecks(["zap_request_timestamp_check"]);
-    table.dropColumn("timestamp");
-  });
 };
