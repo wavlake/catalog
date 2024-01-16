9e847fecd9346fa8bba5f8da7180641d05e27e4f
Josh Remaley <joshremaley@gmail.com>
2024-01-16 17:03:28 -0300

    update schema/migration

--- db/migrations/20240112172608_add_npub.js
+++ db/migrations/20240112172608_add_npub.js
@@ -1,7 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable("npub", function (table) {
-    table.string("npub").primary().unique();
-    table.json("metadata");
+    table.string("public_hex").primary().unique();
+    table.jsonb("metadata");
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 };
 
