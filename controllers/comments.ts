15a9b6cd3e1d55727dd810f6df8f2be43aa9b8a0
joshr4 <joshremaley@gmail.com>
2024-01-25 16:53:26 -0300

    Add album comments (#251)
    * add album comments, refactor comments
    
    * add topMessages to getAlbum
    
    * mv pagination middleware
    
    * use validate lib for id
    
    * flip isValid conditional

--- controllers/comments.ts
+++ controllers/comments.ts
@@ -1,105 +1,51 @@
-import { formatError } from "../library/errors";
 import prisma from "../prisma/client";
 import asyncHandler from "express-async-handler";
 import { getAllComments } from "../library/comments";
+import { formatError } from "../library/errors";
+
+const getCommentsCommon = async (ids, res, next) => {
+  const comments = await getAllComments(ids, 100);
+  if (!comments) {
+    next(formatError(500, "Something went wrong"));
+    return;
+  }
+  res.json({ success: true, data: comments });
+};
 
 const get_comments = asyncHandler(async (req, res, next) => {
-  const { id: contentId } = req.params;
-  if (!contentId) {
-    const error = formatError(400, "Must include a track or episode id");
-    next(error);
-    return;
-  }
-
-  const combinedAndSortedComments = await getAllComments([contentId], 100);
-
-  if (!combinedAndSortedComments) {
-    const error = formatError(500, "Something went wrong");
-    next(error);
-    return;
-  }
-  res.json({
-    success: true,
-    data: combinedAndSortedComments,
-  });
+  await getCommentsCommon([req.params.contentId], res, next);
 });
 
-// looks up all episode ids for a podcast and then gets all comments for those episodes
 const get_podcast_comments = asyncHandler(async (req, res, next) => {
-  const { id: podcastId } = req.params;
-  if (!podcastId) {
-    const error = formatError(400, "Must include the podcast id");
-    next(error);
-    return;
-  }
-
   const episodes = await prisma.episode.findMany({
-    where: { podcastId },
+    where: { podcastId: req.params.podcastId },
   });
 
-  const combinedAndSortedComments = await getAllComments(
-    episodes.map(({ id }) => id),
-    100
-  );
-
-  if (!combinedAndSortedComments) {
-    const error = formatError(500, "Something went wrong");
-    next(error);
-    return;
-  }
-  res.json({ success: true, data: combinedAndSortedComments });
+  const episodeIds = episodes.map(({ id }) => id);
+  await getCommentsCommon(episodeIds, res, next);
 });
 
-// looks up all album ids for an artist, then all the track ids for each album,
-// and then gets all comments for those tracks
 const get_artist_comments = asyncHandler(async (req, res, next) => {
-  const { id: artistId, page = "1", pageSize = "100" } = req.params;
-
-  const pageInt = parseInt(page);
-  if (!Number.isInteger(pageInt) || pageInt <= 0) {
-    const error = formatError(400, "Page must be a positive integer");
-    next(error);
-    return;
-  }
-
-  const pageSizeInt = parseInt(pageSize);
-  if (!Number.isInteger(pageSizeInt) || pageSizeInt <= 0) {
-    const error = formatError(400, "Page size must be a positive integer");
-    next(error);
-    return;
-  }
-
-  const offset = (pageInt - 1) * pageSizeInt; // Calculate the offset
-
-  if (!artistId) {
-    const error = formatError(400, "Must include the artist id");
-    next(error);
-    return;
-  }
-
   const tracks = await prisma.trackInfo.findMany({
-    where: { artistId: artistId },
+    where: { artistId: req.params.artistId },
   });
 
-  const comments = await getAllComments(
-    tracks.map(({ id }) => id),
-    pageSizeInt,
-    offset
-  );
+  const trackIds = tracks.map(({ id }) => id);
+  await getCommentsCommon(trackIds, res, next);
+});
 
-  if (!comments) {
-    const error = formatError(500, "Something went wrong");
-    next(error);
-    return;
-  }
-  res.json({
-    success: true,
-    data: comments,
+const get_album_comments = asyncHandler(async (req, res, next) => {
+  const tracks = await prisma.trackInfo.findMany({
+    where: { albumId: req.params.albumId },
   });
+
+  const trackIds = tracks.map(({ id }) => id);
+  await getCommentsCommon(trackIds, res, next);
 });
 
 export default {
   get_comments,
   get_podcast_comments,
   get_artist_comments,
+  get_album_comments,
 };
