c8014f9fcfe64afdbb4b7f885e0c28f924998ef5
Josh Remaley <joshremaley@gmail.com>
2024-01-29 19:21:37 -0300

    add migration

---
+++ db/migrations/20240129191312_update_invoice_tables.js
@@ -0,0 +1,23 @@
+exports.up = function (knex) {
+  return knex.schema
+    .alterTable("external_receive", function (table) {
+      table.string("external_id").nullable();
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+    .alterTable("external_payment", function (table) {
+      table.boolean("external_id").nullable();
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .alterTable("external_receive", function (table) {
+      table.dropColumn("external_id");
+      table.dropColumn("updated_at");
+    })
+    .alterTable("external_payment", function (table) {
+      table.dropColumn("external_id");
+      table.dropColumn("updated_at");
+    });
+};
