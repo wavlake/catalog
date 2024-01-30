bc8e34cb6cc3fd2b6c3e711c4c48dd2272f4c611
Josh Remaley <joshremaley@gmail.com>
2024-01-30 13:09:17 -0300

    add response payload interfaces

---
+++ library/zbd/interfaces.ts
@@ -0,0 +1,111 @@
+interface BaseResponse {
+  success?: boolean; // Optional because not all interfaces have it
+  message: string;
+}
+
+interface CommonDataFields {
+  id: string;
+  unit: string;
+  createdAt: string;
+  expiresAt: string | null;
+  internalId: string;
+  description: string;
+  callbackUrl: string;
+  status: string;
+}
+
+interface InvoiceBasic {
+  request: string;
+  uri: string;
+}
+
+interface InvoiceExtended extends InvoiceBasic {
+  fastRequest?: string; // Optional because not all invoices have it
+  fastUri?: string; // Optional because not all invoices have it
+}
+
+// Now, refactor the original interfaces to extend these common interfaces:
+
+export interface ZBDCreateStaticCharge extends BaseResponse {
+  data: CommonDataFields & {
+    slots: number;
+    minAmount: string;
+    maxAmount: string;
+    allowedSlots: number;
+    successMessage: string;
+    invoice: InvoiceBasic;
+  };
+}
+
+export interface ZBDCreateWithdrawalRequest extends BaseResponse {
+  success: boolean;
+  data: CommonDataFields & {
+    amount: string;
+    fee: null;
+    invoice: InvoiceExtended;
+  };
+}
+
+export interface ZBDSendKeysendPayment extends BaseResponse {
+  success: boolean;
+  data: {
+    keysendId: string;
+    paymentId: string;
+    transaction: {
+      id: string;
+      walletId: string;
+      type: string;
+      totalAmount: string;
+      fee: string;
+      amount: string;
+      description: string;
+      status: string;
+      confirmedAt: string | null;
+    };
+  };
+}
+
+export interface ZBDCreateChargeLightning extends BaseResponse {
+  success: boolean;
+  data: CommonDataFields & {
+    amount: string;
+    confirmedAt: null;
+    invoice: InvoiceBasic;
+  };
+}
+
+export interface ZBDCreateChargeZBD extends BaseResponse {
+  success: boolean;
+  data: CommonDataFields & {
+    amount: string;
+    invoiceRequest: string;
+    invoiceExpiresAt: string;
+    invoiceDescriptionHash: string | null;
+  };
+}
+
+export interface ZBDPayToLightningAddress extends BaseResponse {
+  success: boolean;
+  data: CommonDataFields & {
+    fee: string;
+    amount: string;
+    invoice: string;
+    preimage: string | null;
+    walletId: string;
+    transactionId: string;
+    comment: string;
+    processedAt: string;
+  };
+}
+
+export interface ZBDSendPayment extends BaseResponse {
+  success: boolean;
+  data: CommonDataFields & {
+    fee: string;
+    amount: string;
+    invoice: string;
+    preimage: string;
+    processedAt: string;
+    confirmedAt: string;
+  };
+}
