40f80d08fdab46ef193723ccbe6d3be2f5e60d05
Michael Rhee <michaelrhee@pm.me>
2024-02-02 16:37:47 -0600

    Update default to false

--- db/migrations/20240202223238_add_is_pending_to_ext_receive.js
+++ db/migrations/20240202223238_add_is_pending_to_ext_receive.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("external_receive", function (table) {
-    table.boolean("is_pending").defaultTo(true);
+    table.boolean("is_pending").defaultTo(false); // Set default to false for existing records
   });
 };
 
