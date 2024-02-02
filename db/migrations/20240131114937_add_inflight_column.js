ec0dcc13e7fec093184d56b96b6eefd4ee814de4
Josh Remaley <joshremaley@gmail.com>
2024-02-01 22:11:20 -0300

    add in_flight to transaction table

--- db/migrations/20240131114937_add_inflight_column.js
+++ db/migrations/20240131114937_add_inflight_column.js
@@ -1,11 +1,19 @@
 exports.up = function (knex) {
-  return knex.schema.alterTable("external_payment", function (table) {
-    table.boolean("in_flight").defaultTo(true);
-  });
+  return knex.schema
+    .alterTable("external_payment", function (table) {
+      table.boolean("in_flight").defaultTo(true);
+    })
+    .alterTable("transaction", function (table) {
+      table.boolean("in_flight").defaultTo(true);
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.alterTable("external_payment", function (table) {
-    table.dropColumn("in_flight");
-  });
+  return knex.schema
+    .alterTable("external_payment", function (table) {
+      table.dropColumn("in_flight");
+    })
+    .alterTable("transaction", function (table) {
+      table.boolean("in_flight");
+    });
 };
