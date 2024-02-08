c950fa20e4a9056923cea712f351fe9f6379df0e
Michael Rhee <michaelrhee@pm.me>
2024-02-08 15:18:07 -0600

    Add artist test case

--- tests/integration/accounting.test.ts
+++ tests/integration/accounting.test.ts
@@ -12,7 +12,9 @@ describe("Accounting integration tests", () => {
     return database.teardown();
   });
 
-  it("amps should adjust all balances accurately", async () => {
+  // NOTE: Since db setup happens only once for all tests, the order of tests matters
+
+  it("adjusts all balances correctly for a basic amp", async () => {
     await amp.processSplits({
       contentId: seeds.testerOneTrackId,
       contentType: "track",
@@ -45,5 +47,40 @@ describe("Accounting integration tests", () => {
     expect(testerOneBalance).toBe("10900");
     expect(testerTwoBalance).toBe("9000");
     expect(trackBalance).toBe("1000");
+  });
+
+  it("adjusts all balances correctly for an artist amp", async () => {
+    await amp.processSplits({
+      contentId: seeds.testerOneArtistId,
+      contentType: "artist",
+      msatAmount: 1000,
+      paymentType: 1,
+      userId: seeds.testerTwoId,
+    });
+
+    const testerOneBalance = await db
+      .knex("user")
+      .where({ id: seeds.testerOneId })
+      .select("msat_balance")
+      .first()
+      .then((user) => user.msat_balance);
+
+    const testerTwoBalance = await db
+      .knex("user")
+      .where({ id: seeds.testerTwoId })
+      .select("msat_balance")
+      .first()
+      .then((user) => user.msat_balance);
+
+    const artistBalance = await db
+      .knex("artist")
+      .where({ id: seeds.testerOneArtistId })
+      .select("msat_total")
+      .first()
+      .then((artist) => artist.msat_total);
+
+    expect(testerOneBalance).toBe("11800");
+    expect(testerTwoBalance).toBe("8000");
+    expect(artistBalance).toBe("1000");
   });
 });
