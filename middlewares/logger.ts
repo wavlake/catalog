8748350abcd2f49533f19f52ff68739d999abf86
Josh Remaley <joshremaley@gmail.com>
2024-02-22 11:08:10 -0600

    use loglevel lib

--- middlewares/logger.ts
+++ middlewares/logger.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Response, Request } from "express";
-
+import log from "loglevel";
 export const logger = (req: Request, res: Response, next: NextFunction) => {
   if (req.method === "OPTIONS" || process.env.NODE_ENV !== "dev") {
     next();
     return;
   }
 
-  console.log(`${req.method} ${req.originalUrl}`);
-  !!Object.entries(req.body).length && console.log(req.body);
+  log.debug(`${req.method} ${req.originalUrl}`);
+  !!Object.entries(req.body).length && log.debug(req.body);
 
   next();
 };
