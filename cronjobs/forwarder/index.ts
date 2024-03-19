0f991d1b295ddeac6754dbd29cdfd8921cc0c67f
Michael Rhee <michaelrhee@pm.me>
2024-03-19 13:32:29 -0500

    Setup container

---
+++ cronjobs/forwarder/index.ts
@@ -0,0 +1,5 @@
+const run = async () => {
+  console.log("Hello");
+};
+
+run();
--- db/migrations/20240319164043_lightning_address.js
+++ db/migrations/20240319164043_lightning_address.js
@@ -1,11 +1,26 @@
 exports.up = function (knex) {
-  return knex.schema.table("user", function (table) {
-    table.text("lightning_address");
-  });
+  return knex.schema
+    .createTable("forward", function (table) {
+      table.increments("id").primary().unique();
+      table.text("user_id").notNullable().index("idx_forward_user_id");
+      table.text("lightning_address").notNullable();
+      table.integer("msat_amount").notNullable();
+      table.boolean("in_flight").notNullable().defaultTo(false);
+      table.boolean("is_settled").notNullable().defaultTo(false);
+      table.text("error").nullable();
+      table.text("external_payment_id").nullable();
+      table.timestamp("created_at").defaultTo(knex.fn.now());
+      table.timestamp("updated_at").defaultTo(knex.fn.now());
+    })
+    .alterTable("user", function (table) {
+      table.text("lightning_address");
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.table("user", function (table) {
-    table.dropColumn("lightning_address");
-  });
+  return knex.schema
+    .table("user", function (table) {
+      table.dropColumn("lightning_address");
+    })
+    .dropTable("forward");
 };
