b6e9e8cce65f5d4aa2424fa532fa58c08f6d5db2
Michael Rhee <michaelrhee@pm.me>
2024-03-21 14:41:30 -0500

    Update ip miiddleware

--- services/accounting/index.ts
+++ services/accounting/index.ts
@@ -13,10 +13,11 @@ const corsHost = process.env.CORS_HOST;
 log.setLevel(process.env.LOGLEVEL || "info");
 const sentryDsn = process.env.SENTRY_DSN;
 const sentryTracesSampleRate = process.env.SENTRY_TRACES_SAMPLE_RATE;
+const proxyCount = parseInt(process.env.PROXY_COUNT) || 2;
 
 // To obtain the client's IP address from behind the nginx proxy
 // Info: https://expressjs.com/en/guide/behind-proxies.html
-app.set("trust proxy", 2);
+app.set("trust proxy", proxyCount);
 
 Sentry.init({
   dsn: sentryDsn,
--- services/accounting/routes/callback.ts
+++ services/accounting/routes/callback.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import callbackController from "../controllers/callback";
+const { isZbdIp } = require("@middlewares/zbdChecks");
 
 // Create router
 const router = express.Router();
@@ -7,10 +8,26 @@ const router = express.Router();
 //////// ROUTES ////////
 
 // TODO: Add ZBD IP check to all these routes
-router.post("/receive/keysend", callbackController.processIncomingKeysend);
-router.post("/send/keysend", callbackController.processOutgoingKeysend);
-router.post("/receive/invoice", callbackController.processIncomingInvoice);
-router.post("/send/invoice", callbackController.processOutgoingInvoice);
+router.post(
+  "/receive/keysend",
+  isZbdIp,
+  callbackController.processIncomingKeysend
+);
+router.post(
+  "/send/keysend",
+  isZbdIp,
+  callbackController.processOutgoingKeysend
+);
+router.post(
+  "/receive/invoice",
+  isZbdIp,
+  callbackController.processIncomingInvoice
+);
+router.post(
+  "/send/invoice",
+  isZbdIp,
+  callbackController.processOutgoingInvoice
+);
 
 // Export router
 export default router;
