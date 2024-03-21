45bcd7cd4ebbb63dfb82766f7489041da45e33ea
Josh Remaley <joshremaley@gmail.com>
2024-03-21 15:11:11 -0500

    rm old function

--- controllers/episodes.ts
+++ controllers/episodes.ts
@@ -269,23 +269,6 @@ export const update_episode = asyncHandler(async (req, res, next) => {
     }
   }
 
-  const calculatedPublishedAt = () => {
-    // now in server UTC time
-    const now = new Date();
-    // TODO - consume the date from the request when scheduling is implemented
-    const scheduledDate = new Date();
-
-    // if the track is being published (isDraft being changed from true to false) set the publishedAt field to the current time
-    if (unEditedEpisode.isDraft === true && isDraft === false) {
-      return now;
-    }
-
-    // if the track is being unpublished (isDraft being changed from false to true) set the publishedAt field to undefined
-    if (unEditedEpisode.isDraft === false && isDraft === true) {
-      return undefined;
-    }
-  };
-
   log.debug(`Editing episode ${episodeId}`);
   const updatedEpisode = await prisma.episode.update({
     where: {
@@ -297,7 +280,6 @@ export const update_episode = asyncHandler(async (req, res, next) => {
       updatedAt,
       description,
       isExplicit,
-      publishedAt: calculatedPublishedAt(),
     },
   });
 
