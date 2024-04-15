8bb8377e7f504a740aff78d5d167506949eeb086
joshr4 <joshremaley@gmail.com>
2024-04-15 15:16:41 -0500

    Update db/migrations/20240411212105_add_ext_user_table.js
    Co-authored-by: Michael Rhee <77257032+blastshielddown@users.noreply.github.com>

--- db/migrations/20240411212105_add_ext_user_table.js
+++ db/migrations/20240411212105_add_ext_user_table.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("external_user", function (table) {
-    table.string("external_id").notNullable().primary().unique();
+    table.string("external_id", 64).notNullable().primary().unique();
     table.string("firebase_uid", 64);
     table.string("provider", 128);
   });
