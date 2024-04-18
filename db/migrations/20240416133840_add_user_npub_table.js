ac1ee67ffa31d148684ea8b8f4750de724060371
Josh Remaley <joshremaley@gmail.com>
2024-04-18 16:27:00 -0500

    ad fk constraint to user_verification table

--- db/migrations/20240416133840_add_user_npub_table.js
+++ db/migrations/20240416133840_add_user_npub_table.js
@@ -1,16 +1,24 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("user_pubkey", function (table) {
-    table.text("pubkey", 64).unique().primary();
-    table
-      .text("user_id", 64)
-      .notNullable()
-      .references("id")
-      .inTable("user")
-      .index("idx_user_pubkey_user_id");
-    table.datetime("created_at").notNullable();
-  });
+  return knex.schema
+    .createTable("user_pubkey", function (table) {
+      table.text("pubkey", 64).unique().primary();
+      table
+        .text("user_id", 64)
+        .notNullable()
+        .references("id")
+        .inTable("user")
+        .index("idx_user_pubkey_user_id");
+      table.datetime("created_at").notNullable();
+    })
+    .alterTable("user_verification", function (table) {
+      table.foreign("user_id").references("user.id");
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("user_pubkey");
+  return knex.schema
+    .dropTableIfExists("user_pubkey")
+    .alterTable("user_verification", function (table) {
+      table.dropForeign("user_id");
+    });
 };
