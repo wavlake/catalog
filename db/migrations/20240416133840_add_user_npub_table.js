c41f568db5295fb54d9caac174fcc21f70f76067
Josh Remaley <joshremaley@gmail.com>
2024-04-18 15:32:04 -0500

    add fk constraint to user_id

--- db/migrations/20240416133840_add_user_npub_table.js
+++ db/migrations/20240416133840_add_user_npub_table.js
@@ -1,7 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable("user_pubkey", function (table) {
     table.text("pubkey", 64).unique().primary();
-    table.text("user_id", 64);
+    table
+      .text("user_id", 64)
+      .notNullable()
+      .references("id")
+      .inTable("user")
+      .index("idx_user_pubkey_user_id");
     table.datetime("created_at").notNullable();
   });
 };
