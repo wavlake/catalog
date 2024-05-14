150f734d1527787a628540ef9a5a1a0c48ab8413
Michael Rhee <michaelrhee@pm.me>
2024-05-14 15:18:41 -0500

    Fix hybrid auth

--- middlewares/nostrAuth.ts
+++ middlewares/nostrAuth.ts
@@ -5,38 +5,41 @@ import { formatError } from "../library/errors";
 
 // This middleware is used to authenticate requests from Nostr
 // It follows the NIP-98 spec - https://github.com/nostr-protocol/nips/blob/master/98.md
+export const validateNostrEvent = async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    throw "Missing authorization header";
+  }
+
+  const nostrEvent = await nip98.unpackEventFromToken(authorization);
+
+  const host = req.get("host");
+  // req.protocol may be "http" when express is behind a proxy or load balancer
+  // so we hardcode it as HTTPS here, since that is what the client will be using
+  const fullUrl = `${
+    host.includes("localhost") ? req.protocol : "https"
+  }://${host}${req.originalUrl}`;
+
+  const eventIsValid = await nip98.validateEvent(
+    nostrEvent,
+    fullUrl,
+    req.method,
+    req.body
+  );
+  if (!eventIsValid) {
+    throw "Invalid event";
+  }
+
+  // successfull auth'd, add the event to res.locals so other middleware can use it
+  // https://expressjs.com/en/api.html#res.locals
+  res.locals.authEvent = nostrEvent;
+};
+
 export const isNostrAuthorized = asyncHandler(async (req, res, next) => {
-  try {
-    const { authorization } = req.headers;
-    if (!authorization) {
-      throw "Missing authorization header";
-    }
-
-    const nostrEvent = await nip98.unpackEventFromToken(authorization);
-
-    const host = req.get("host");
-    // req.protocol may be "http" when express is behind a proxy or load balancer
-    // so we hardcode it as HTTPS here, since that is what the client will be using
-    const fullUrl = `${
-      host.includes("localhost") ? req.protocol : "https"
-    }://${host}${req.originalUrl}`;
-
-    const eventIsValid = await nip98.validateEvent(
-      nostrEvent,
-      fullUrl,
-      req.method,
-      req.body
-    );
-    if (!eventIsValid) {
-      throw "Invalid event";
-    }
-
-    // successfull auth'd, add the event to res.locals so other middleware can use it
-    // https://expressjs.com/en/api.html#res.locals
-    res.locals.authEvent = nostrEvent;
-    next();
-  } catch (err) {
+  await validateNostrEvent(req, res).catch((err) => {
     const error = formatError(401, err);
     throw error;
-  }
+  });
+
+  next();
 });
