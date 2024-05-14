fc885147ab8de53fff439f426e2f38c26f5ff8a3
joshr4 <joshremaley@gmail.com>
2024-05-14 14:41:30 -0500

    Update playlist controllers (#396)
    * clean up playlist logic
    
    * create composite auth middleware
    
    * Squashed commit of the following:
    
    commit cf3433015978eab52d963adf05fa7a81260fc47e
    Author: Josh Remaley <joshremaley@gmail.com>
    Date:   Wed May 1 15:36:51 2024 -0500
    
        add migration and update playlist controllers
    
    commit 20b600502443300008475b4ab1aa17d1c5a50c63
    Author: Josh Remaley <joshremaley@gmail.com>
    Date:   Wed May 1 13:41:26 2024 -0500
    
        update random endpoint controller
    
    * udpate schema
    
    * remove lines from schema
    
    * update reorder controller
    
    * fix schema
    
    * fix schema
    
    * add content owner check
    
    * improve composite auth middleware
    
    * rm log
    
    * add comment

---
+++ middlewares/auth.ts
@@ -0,0 +1,42 @@
+import { auth } from "../library/firebaseService";
+import Sentry from "@sentry/node";
+
+import { formatError } from "../library/errors";
+import asyncHandler from "express-async-handler";
+
+export const isFirebaseAuthorized = async (req) => {
+  let authToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    try {
+      authToken = req.headers.authorization.split(" ")[1];
+    } catch (err) {
+      return Promise.reject("Authentication failed, error parsing token");
+    }
+  } else {
+    return Promise.reject("Missing authorization token");
+  }
+
+  try {
+    const user = await auth().verifyIdToken(authToken);
+    req.uid = user.uid;
+    req.params.uid = user.uid;
+    return user;
+  } catch (err) {
+    return Promise.reject("Authentication failed");
+  }
+};
+
+export const isAuthorized = asyncHandler(async (req, res, next) => {
+  try {
+    const user = await isFirebaseAuthorized(req);
+    console.log(user);
+    if (!user) return next(formatError(500, "Authentication failed"));
+    next();
+  } catch (err) {
+    Sentry.captureException(err);
+    next(err);
+  }
+});
--- middlewares/nostrAuth.ts
+++ middlewares/nostrAuth.ts
@@ -66,58 +66,57 @@ async function validateEvent(
 
 // This middleware is used to authenticate requests from Nostr
 // It follows the NIP-98 spec - https://github.com/nostr-protocol/nips/blob/master/98.md
-export const isNostrAuthorized = asyncHandler(async (req, res, next) => {
-  try {
-    const { authorization } = req.headers;
-    if (!authorization) {
-      const error = formatError(401, "Missing authorization header");
-      next(error);
-      return;
-    }
-
-    const nostrEvent = await nip98.unpackEventFromToken(authorization);
-
-    const host = req.get("host");
-    // req.protocol may be "http" when express is behind a proxy or load balancer
-    // so we hardcode it as HTTPS here, since that is what the client will be using
-    const fullUrl = `${
-      host.includes("localhost") ? req.protocol : "https"
-    }://${host}${req.originalUrl}`;
-
-    const eventIsValid = await validateEvent(
-      nostrEvent,
-      fullUrl,
-      req.method,
-      req.body
-    );
-
-    // TODO- replace with nip98.validateEvent, imported from nostr-tools
-    if (!eventIsValid) {
-      const error = formatError(401, "Invalid event");
-      next(error);
-      return;
-    }
-
-    // TODO - move this to be triggered elsewhere (e.g. when a zap invoice is paid), once server migration to catalog is complete
-    // async update the npub metadata in the db
-    updateNpubMetadata(nostrEvent.pubkey)
-      .then(({ isSuccess }) => {
-        log.debug(
-          `${isSuccess ? "Updated" : "Failed to update"} nostr metadata for: ${
-            nostrEvent.pubkey
-          }`
-        );
-      })
-      .catch((err) => {
-        log.debug("error updating npub metadata: ", err);
-      });
-
-    // successfull auth'd, add the event to res.locals so other middleware can use it
-    // https://expressjs.com/en/api.html#res.locals
-    res.locals.authEvent = nostrEvent;
-    next();
-  } catch (err) {
-    const error = formatError(401, err);
-    next(error);
+export const validateNostrEvent = async (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    throw "Missing authorization header";
   }
+
+  const nostrEvent = await nip98.unpackEventFromToken(authorization);
+
+  const host = req.get("host");
+  // req.protocol may be "http" when express is behind a proxy or load balancer
+  // so we hardcode it as HTTPS here, since that is what the client will be using
+  const fullUrl = `${
+    host.includes("localhost") ? req.protocol : "https"
+  }://${host}${req.originalUrl}`;
+
+  const eventIsValid = await validateEvent(
+    nostrEvent,
+    fullUrl,
+    req.method,
+    req.body
+  );
+
+  // TODO- replace with nip98.validateEvent, imported from nostr-tools
+  if (!eventIsValid) {
+    throw "Invalid event";
+  }
+
+  // TODO - move this to be triggered elsewhere (e.g. when a zap invoice is paid), once server migration to catalog is complete
+  // async update the npub metadata in the db
+  updateNpubMetadata(nostrEvent.pubkey)
+    .then(({ isSuccess }) => {
+      log.debug(
+        `${isSuccess ? "Updated" : "Failed to update"} nostr metadata for: ${
+          nostrEvent.pubkey
+        }`
+      );
+    })
+    .catch((err) => {
+      log.debug("error updating npub metadata: ", err);
+    });
+
+  // successfull auth'd, add the event to res.locals so other middleware can use it
+  // https://expressjs.com/en/api.html#res.locals
+  res.locals.authEvent = nostrEvent;
+};
+
+export const isNostrAuthorized = asyncHandler(async (req, res, next) => {
+  await validateNostrEvent(req, res).catch((err) => {
+    const error = formatError(401, err);
+    throw error;
+  });
+
+  next();
 });
