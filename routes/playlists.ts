5fe44f57fd575de26998652bea0186c6df63255d
Josh Remaley <joshremaley@gmail.com>
2024-05-21 11:20:13 -0500

    update comment

--- routes/playlists.ts
+++ routes/playlists.ts
@@ -18,7 +18,7 @@ const router = express.Router();
 // queries
 router.get("/:id", getPlaylist);
 router.get("/", isFirebaseOrNostrAuthorized, getUserPlaylists);
-// public route?
+// public route to get a user's playlists
 router.get("/user/:id", getUserPlaylists);
 
 // mutations
