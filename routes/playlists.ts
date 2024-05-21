1bb6380395f64534a74f25d94f8daf6e0e072938
Josh Remaley <joshremaley@gmail.com>
2024-05-21 10:46:14 -0500

    add new user playlist route

--- routes/playlists.ts
+++ routes/playlists.ts
@@ -18,6 +18,8 @@ const router = express.Router();
 // queries
 router.get("/:id", getPlaylist);
 router.get("/", isFirebaseOrNostrAuthorized, getUserPlaylists);
+// public route?
+router.get("/user/:id", getUserPlaylists);
 
 // mutations
 router.post("/add-track", isFirebaseOrNostrAuthorized, addTrackToPlaylist);
