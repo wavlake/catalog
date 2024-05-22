8b0e40c588a9e919f43a4cdaa65b3094adc447a0
Josh Remaley <joshremaley@gmail.com>
2024-05-22 15:40:01 -0500

    update migration

--- db/migrations/20240522152908_add_follower_info.js
+++ db/migrations/20240522152908_add_follower_info.js
@@ -1,13 +1,13 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("user_pubkey", function (table) {
-    table.jsonb("followers");
+    table.integer("follower_count").defaultTo(0);
     table.jsonb("follows");
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.alterTable("user_pubkey", function (table) {
-    table.dropColumn("followers");
+    table.dropColumn("follower_count");
     table.dropColumn("follows");
   });
 };
