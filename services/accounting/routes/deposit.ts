1fa6c402ee853effbd263a8c4d7a1078c0ed124d
Michael Rhee <michaelrhee@pm.me>
2024-05-22 12:07:06 -0500

    Update deposit logic

--- services/accounting/routes/deposit.ts
+++ services/accounting/routes/deposit.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import depositController from "../controllers/deposit";
 const { isAuthorized } = require("@middlewares/auth");
-const { isZbdRegion } = require("@middlewares/zbdChecks");
 
 // Create router
 const router = express.Router();
 
 //////// ROUTES ////////
 
-router.post("/", isAuthorized, isZbdRegion, depositController.createDeposit);
+router.post("/", isAuthorized, depositController.createDeposit);
+router.get("/:id", isAuthorized, depositController.getDeposit);
 
 // Export router
 export default router;
