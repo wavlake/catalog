2f44dac8056062a729449899e6fca781253fafba
Michael Rhee <michaelrhee@pm.me>
2024-06-26 09:58:36 -0500

    Handle NaN

--- cronjobs/forwarder/index.ts
+++ cronjobs/forwarder/index.ts
@@ -217,7 +217,7 @@ const handleReconciliation = async (uniqueExternalPaymentIds: string[]) => {
       status: status as PaymentStatus,
       preimage: preimage,
       msatAmount: parseInt(amount),
-      fee: parseInt(fee),
+      fee: isNaN(parseInt(fee)) ? 0 : parseInt(fee),
     });
   }
   return;
