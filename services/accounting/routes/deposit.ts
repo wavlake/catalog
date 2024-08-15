f3dd659a3da32e59f11c4cdaff250bb7e19b3729
Michael Rhee <michaelrhee@pm.me>
2024-08-15 09:42:45 -0500

    Add limit to lnurl route

--- services/accounting/routes/deposit.ts
+++ services/accounting/routes/deposit.ts
@@ -3,9 +3,20 @@ import depositController from "../controllers/deposit";
 import { isAuthorized } from "@middlewares/auth";
 import { isWalletVerified } from "@middlewares/zbdChecks";
 import { isAPITokenAuthorized } from "@middlewares/isAPITokenAuthorized";
+const { rateLimit } = require("express-rate-limit");
 
 // Create router
 const router = express.Router();
+
+// Rate limit
+const rateTimeWindow = 10000; // 10 seconds
+const limiter = rateLimit({
+  windowMs: rateTimeWindow,
+  max: 2, // Limit each lnurl address to this many requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+  keyGenerator: (req, res) => req.body.userId,
+});
 
 //////// ROUTES ////////
 
@@ -20,6 +31,7 @@ router.post(
   "/lnurl",
   isAPITokenAuthorized,
   isWalletVerified,
+  limiter,
   depositController.createDepositLNURL
 );
 
