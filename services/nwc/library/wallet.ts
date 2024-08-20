8b020af3965da9616c19550154233bbd2aa3c3e3
Josh Remaley <joshremaley@gmail.com>
2024-08-19 19:04:10 -0500

    handle zero budget (unlimited) for nwc

--- services/nwc/library/wallet.ts
+++ services/nwc/library/wallet.ts
@@ -61,6 +61,11 @@ const walletHasRemainingBudget = async (
     .andWhere("created_at", ">", db.knex.raw("now() - interval '7 days'"))
     .first()
     .then((data) => {
+      // if the max budget is 0 then the user has no budget, its unlimited
+      if (msatBudget === 0) {
+        return true;
+      }
+
       // If there are no tx records then simply check if the budget is greater than the value
       if (data?.length === 0) {
         return parseInt(msatBudget) > parseInt(valueMsat);
