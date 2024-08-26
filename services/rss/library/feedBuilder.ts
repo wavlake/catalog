e265a243b0a03445fa3413bcfcac6366052c7538
Michael Rhee <michaelrhee@pm.me>
2024-08-26 13:34:19 -0500

    Add op3 prefix

--- services/rss/library/feedBuilder.ts
+++ services/rss/library/feedBuilder.ts
@@ -10,6 +10,8 @@ const {
   valueTimeSplit,
 } = require("./rssUtils");
 
+const OP3_PREFIX = "https://op3.dev/e/";
+
 const buildAlbumFeed = async (data) => {
   const [
     {
@@ -360,7 +362,11 @@ const buildPodcastFeed = async (data) => {
           date: createDate, // any format that js Date can parse.
           // lat: 33.417974, //optional latitude field for GeoRSS
           // long: -111.933231, //optional longitude field for GeoRSS
-          enclosure: { url: liveUrl, size: size, type: "audio/mpeg" },
+          enclosure: {
+            url: `${OP3_PREFIX}${liveUrl}`,
+            size: size,
+            type: "audio/mpeg",
+          },
           // itunesAuthor: '',
           // itunesExplicit: false,
           // itunesSubtitle: '',
