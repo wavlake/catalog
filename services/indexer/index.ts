58a5a462e57d994e44355eb8cb43be51cbf9e10e
Michael Rhee <michaelrhee@pm.me>
2024-09-04 15:34:10 -0500

    Update indexer, move rssUtils library"

--- services/indexer/index.ts
+++ services/indexer/index.ts
@@ -1,9 +1,21 @@
 require("dotenv").config();
 import log, { LogLevelDesc } from "loglevel";
 log.setLevel(process.env.LOGLEVEL as LogLevelDesc);
-import { getPublicKey, Relay, nip04 } from "nostr-tools";
+import { getPublicKey, finalizeEvent, UnsignedEvent } from "nostr-tools";
 const { useWebSocketImplementation, SimplePool } = require("nostr-tools/pool");
-import { getContentFromId } from "@library/content";
+import {
+  getContentFromId,
+  getParentContentTypeAndId,
+  getType,
+} from "@library/content";
+const {
+  feedPath,
+  receivingPublicKey,
+  podcastNamespace,
+  valueRecipient,
+  valueTimeSplit,
+} = require("@library/rssUtils");
+const { v5, validate } = require("uuid");
 import { DEFAULT_READ_RELAY_URIS } from "@library/nostr/common";
 import { hexToBytes } from "@noble/hashes/utils"; // already an installed dependency
 useWebSocketImplementation(require("ws"));
@@ -14,8 +26,9 @@ const pool = new SimplePool();
 const relayUris = DEFAULT_READ_RELAY_URIS;
 const walletSk = process.env.WALLET_SERVICE_SECRET;
 
-const walletSkHex = hexToBytes(walletSk);
-const walletServicePubkey = getPublicKey(walletSkHex);
+const walletSkBytes = hexToBytes(walletSk);
+const walletServicePubkey = getPublicKey(walletSkBytes);
+const LABEL_NAMESPACE = "org.podcastindex";
 
 // Main process
 const main = async () => {
@@ -23,7 +36,7 @@ const main = async () => {
   if (!walletSk) {
     throw new Error("No wallet service SK found");
   }
-  const sub = pool.subscribeMany(
+  pool.subscribeMany(
     relayUris,
     [
       {
@@ -35,6 +48,7 @@ const main = async () => {
     {
       onevent: (event: any) => {
         // log.debug("Received event");
+        // log.debug(`Received event: ${JSON.stringify(event)}`);
         // this will only be called once the first time the event is received
         checkEvent(event);
       },
@@ -47,24 +61,80 @@ const main = async () => {
 };
 
 const checkEvent = async (event: any) => {
-  if (event.kind === 1) {
-    const content = event.content;
-    // See if a wavlake.com link is in the content
-    if (content.includes("wavlake.com")) {
-      log.debug(`Found Wavlake link...`);
-      // Look for a uuid in the content
-      const uuid = content.match(
-        /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/
-      )[0];
-      log.debug(`Found uuid: ${uuid}`);
-      // Fetch the content id
-      const wavlakeContent = await getContentFromId(uuid);
-      // Send the content to the wallet service
-      if (wavlakeContent) {
-        log.debug(`Found content: ${JSON.stringify(wavlakeContent)}`);
-      }
+  if (event.kind === 1 && event.content.includes("wavlake.com")) {
+    const eventContent = event.content;
+    log.debug(`Found Wavlake link...`);
+    // Look for a uuid in the content
+    const uuidMatch = eventContent.match(
+      /([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})/
+    );
+    if (!uuidMatch) {
+      log.debug("No uuid found in content");
+      return;
+    }
+    const contentId = uuidMatch[0];
+    // log.debug(`Found uuid: ${contentId}`);
+    // Fetch the content id
+    const wavlakeContent = await getContentFromId(contentId);
+    // Send the content to the wallet service
+    if (wavlakeContent) {
+      // log.debug(`Found content: ${JSON.stringify(wavlakeContent)}`);
+      const contentType = await getType(contentId);
+      const parentContentData = await getParentContentTypeAndId(contentId);
+      await publishLabelEvent(
+        event.id,
+        contentId,
+        contentType,
+        parentContentData
+      );
+      return;
     }
   }
 };
 
+const publishLabelEvent = async (
+  referencedEventId: string,
+  contentId: string,
+  contentType: string,
+  parentContentData: any
+) => {
+  const itemLabel = await generateIdentifier(contentId, contentType);
+  const parentItemLabel = await generateIdentifier(
+    parentContentData.parentId,
+    parentContentData.contentType
+  );
+
+  const eventTemplate: UnsignedEvent = {
+    kind: 1985,
+    pubkey: walletServicePubkey,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      ["L", LABEL_NAMESPACE],
+      ["l", itemLabel, LABEL_NAMESPACE],
+      ["e", referencedEventId],
+      ["i", itemLabel],
+      ...(parentItemLabel ? [["i", parentItemLabel]] : []),
+    ],
+    content: "",
+  };
+  const signedEvent = finalizeEvent(eventTemplate, walletSkBytes);
+  log.debug(`Publishing label event: ${JSON.stringify(signedEvent)}`);
+  await Promise.any(pool.publish(relayUris, signedEvent));
+};
+
+const generateIdentifier = async (contentId: string, contentType: string) => {
+  if (contentType === "track" || contentType === "episode") {
+    return `podcast:item:guid:${contentId}`;
+  } else if (contentType === "album" || contentType === "podcast") {
+    return `podcast:guid:${v5(
+      feedPath(contentType, contentId),
+      podcastNamespace
+    )}`;
+  } else if (contentType === "artist") {
+    return `podcast:publisher:guid:${contentId}`;
+  } else {
+    log.debug(`Unknown content type: ${contentType}`);
+    return;
+  }
+};
 main().catch((err) => log.error(err));
