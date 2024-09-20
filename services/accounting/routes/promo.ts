f248f9dcde1f2b3346402547eebaebdbb7296dd5
Michael Rhee <michaelrhee@pm.me>
2024-09-20 14:16:28 -0500

    Update rate limit

--- services/accounting/routes/promo.ts
+++ services/accounting/routes/promo.ts
@@ -5,7 +5,9 @@ const { isWalletVerified } = require("@middlewares/zbdChecks");
 const { rateLimit } = require("express-rate-limit");
 
 const env = process.env.NODE_ENV || "dev";
-// This limit is set to allow only 1 request per minute, with some wiggle room
+// This limit is set to allow only 2 requests per 28 second window
+// This allows a client to create a promo reward, update it, and then create another one every minute-ish
+// But that's it. This is to prevent abuse.
 const rateTimeWindow = env === "dev" ? 2000 : 28000; // 10 seconds in dev, 28000 seconds in prod
 
 // Create router
@@ -14,7 +16,7 @@ const router = express.Router();
 // Rate limit
 const limiter = rateLimit({
   windowMs: rateTimeWindow,
-  max: 1, // Limit each user to 1 requests per `window`
+  max: 2, // Limit each user to 1 requests per `window`
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
   keyGenerator: (req, res) => req["uid"],
