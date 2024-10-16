7bcb73b5c0912bb96edc25eaf0b4dc4e01bade95
Josh Remaley <joshremaley@gmail.com>
2024-10-16 08:43:25 -0500

    add dupe name check for artist create

--- controllers/artists.ts
+++ controllers/artists.ts
@@ -122,6 +122,19 @@ const create_artist = asyncHandler(async (req, res, next) => {
     return;
   }
 
+  const artistExists = await prisma.artist.findFirst({
+    where: { artistUrl: urlFriendly(request.name) },
+  });
+
+  if (artistExists) {
+    const error = formatError(
+      403,
+      "Artist name already exists, please choose another name."
+    );
+    next(error);
+    return;
+  }
+
   const cdnImageUrl = await upload_image(
     request.artwork,
     newArtistId,
