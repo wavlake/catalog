0f5ef3f5376dc659ab9bf06e6c60bde558654c7d
Michael Rhee <77257032+blastshielddown@users.noreply.github.com>
2024-10-29 09:40:32 -0500

    Shuffle promos (#606)
    * Move shuffle function, add to promos
    
    * Rename const

--- library/helpers.ts
+++ library/helpers.ts
@@ -24,3 +24,13 @@ export const getStatus = (isDraft: boolean, isPublished: Date): Status => {
   const publishedTime = new Date(isPublished);
   return publishedTime > new Date() ? Status.scheduled : Status.published;
 };
+
+// Durstenfeld Shuffle, via: https://stackoverflow.com/a/12646864
+export const shuffle = (trackList) => {
+  const newArray = trackList as any[];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
