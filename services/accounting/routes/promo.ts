138057ad8e896a8956e417a896ebdde2aee28b4a
Michael Rhee <michaelrhee@pm.me>
2024-10-30 12:08:06 -0500

    Add ip rate limit

--- services/accounting/routes/promo.ts
+++ services/accounting/routes/promo.ts
@@ -5,15 +5,13 @@ const { isWalletVerified } = require("@middlewares/zbdChecks");
 const { rateLimit } = require("express-rate-limit");
 
 const env = process.env.NODE_ENV || "dev";
-// This limit is set to allow only 2 requests per 28 second window
-// This allows a client to create a promo reward, update it, and then create another one every minute-ish
-// But that's it. This is to prevent abuse.
+// This limit is set to allow only 1 requests per 58 second window to prevent abuse.
 const rateTimeWindow = env === "dev" ? 2000 : 58000; // 10 seconds in dev, 58000 seconds in prod
 
 // Create router
 const router = express.Router();
 
-// Rate limit
+// User rate limit
 const limiter = rateLimit({
   windowMs: rateTimeWindow,
   max: 1, // Limit each user to 1 requests per `window`
@@ -22,12 +20,21 @@ const limiter = rateLimit({
   keyGenerator: (req, res) => req["uid"],
 });
 
+// IP rate limit
+const iplimiter = rateLimit({
+  windowMs: rateTimeWindow,
+  max: 1, // Limit each user to 1 requests per `window`
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+});
+
 //////// ROUTES ////////
 
 router.post(
   "/reward",
   isAuthorized,
   limiter,
+  iplimiter,
   isWalletVerified,
   promoController.createPromoReward
 );
