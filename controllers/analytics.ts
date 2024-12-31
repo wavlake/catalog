0ec87dce9fbe1bf07991c8158001534f6e4499ba
Josh Remaley <joshremaley@gmail.com>
2024-12-31 12:19:27 -0600

    fix op3 data merging and stats

--- controllers/analytics.ts
+++ controllers/analytics.ts
@@ -2,7 +2,7 @@ import log from "../library/winston";
 import asyncHandler from "express-async-handler";
 import Sentry from "@sentry/node";
 import { validate } from "uuid";
-import { getContentStats } from "../library/op3";
+import { getContentStats, OP3CombinedData } from "../library/op3";
 import {
   getEarningsNumbers,
   getTopSupporters,
@@ -10,38 +10,49 @@ import {
   getLifetimeEarnings,
   getContentMonthlyEarnings,
 } from "../library/analytics";
+import { ResponseObject } from "../types/catalogApi";
+
+type Downloads = {
+  earningsMsat: number;
+  uniqueAmpUsers: number;
+} & OP3CombinedData;
 
 // Get the last 30 days of download stats for a content item
 // plus earnings for that item
-const get_downloads = asyncHandler(async (req, res, next) => {
-  const userId = req["uid"];
-  const contentId = req.query.contentId;
-  if (typeof contentId !== "string" || !validate(contentId)) {
-    res.status(400).json({ success: false, error: "contentId is required" });
-    return;
-  }
+const get_downloads = asyncHandler<{}, ResponseObject<Downloads>, any>(
+  async (req, res, next) => {
+    const userId = req["uid"];
+    const contentId = req.query.contentId;
+    if (typeof contentId !== "string" || !validate(contentId)) {
+      res.status(400).json({ success: false, error: "contentId is required" });
+      return;
+    }
 
-  const startDate = req.query.startDate as string;
-  // check if startDate is a valid ISO date
-  if (startDate && !Date.parse(startDate)) {
-    res.status(400).json({
-      success: false,
-      error: "startDate must be a valid ISO date string (yyyy-mm-dd)",
+    const startDate = req.query.startDate as string;
+    // check if startDate is a valid ISO date
+    if (startDate && !Date.parse(startDate)) {
+      res.status(400).json({
+        success: false,
+        error: "startDate must be a valid ISO date string (yyyy-mm-dd)",
+      });
+      return;
+    }
+    const response = await getContentStats(contentId, startDate);
+
+    const earnings = await getContentMonthlyEarnings(userId, contentId);
+    res.json({
+      success: true,
+      data: {
+        rows: response.rows ?? [],
+        count: response.count ?? 0,
+        statsPageUrl: response.statsPageUrl ?? "",
+        releaseTitle: response.releaseTitle ?? "",
+        earningsMsat: earnings.earnings ?? 0,
+        uniqueAmpUsers: earnings.uniqueAmpUsers ?? 0,
+      },
     });
-    return;
   }
-  const response = await getContentStats(contentId, startDate);
-
-  const earnings = await getContentMonthlyEarnings(userId, contentId);
-  res.json({
-    success: true,
-    data: {
-      ...response,
-      earningsMsat: earnings.earnings,
-      uniqueAmpUsers: earnings.uniqueAmpUsers,
-    },
-  });
-});
+);
 
 const get_earnings = asyncHandler(async (req, res, next) => {
   const userId = req["uid"];
