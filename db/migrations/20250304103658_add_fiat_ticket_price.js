07b2f05d4d1b3090bd9ea2eea6aeb337ef7a455c
Josh Remaley <joshremaley@gmail.com>
2025-03-05 09:25:11 -0600

    add precision to fiat ticket price field in ticketed_event table

--- db/migrations/20250304103658_add_fiat_ticket_price.js
+++ db/migrations/20250304103658_add_fiat_ticket_price.js
@@ -1,7 +1,10 @@
 exports.up = function (knex) {
   return knex.schema.alterTable("ticketed_event", function (table) {
-    table.decimal("price_fiat").nullable();
+    table.decimal("price_fiat", 10, 2).nullable();
     table.string("currency", 3).nullable();
+    table.raw(
+      "ALTER TABLE ticketed_event ADD CONSTRAINT chk_currency_required CHECK ((price_fiat IS NULL AND currency IS NULL) OR (price_fiat IS NOT NULL AND currency IS NOT NULL))"
+    );
   });
 };
 
