9b8d5b7bcf7f7f0f60ee6c47d5c5b76034b86841
Josh Remaley <joshremaley@gmail.com>
2025-03-06 10:37:39 -0600

    fix the migration for ticket fiat price

--- db/migrations/20250304103658_add_fiat_ticket_price.js
+++ db/migrations/20250304103658_add_fiat_ticket_price.js
@@ -1,16 +1,23 @@
 exports.up = function (knex) {
-  return knex.schema.alterTable("ticketed_event", function (table) {
-    table.decimal("price_fiat", 10, 2).nullable();
-    table.string("currency", 3).nullable();
-    table.raw(
-      "ALTER TABLE ticketed_event ADD CONSTRAINT chk_currency_required CHECK ((price_fiat IS NULL AND currency IS NULL) OR (price_fiat IS NOT NULL AND currency IS NOT NULL))"
-    );
-  });
+  return knex.schema
+    .alterTable("ticketed_event", function (table) {
+      table.decimal("price_fiat", 10, 2).nullable();
+      table.string("currency", 3).nullable();
+    })
+    .then(() => {
+      return knex.raw(
+        "ALTER TABLE ticketed_event ADD CONSTRAINT chk_currency_required CHECK ((price_fiat IS NULL AND currency IS NULL) OR (price_fiat IS NOT NULL AND currency IS NOT NULL))"
+      );
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.alterTable("ticketed_event", function (table) {
-    table.dropColumn("price_fiat");
-    table.dropColumn("currency");
-  });
+  return knex
+    .raw("ALTER TABLE ticketed_event DROP CONSTRAINT chk_currency_required")
+    .then(() => {
+      return knex.schema.alterTable("ticketed_event", function (table) {
+        table.dropColumn("price_fiat");
+        table.dropColumn("currency");
+      });
+    });
 };
