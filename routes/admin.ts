fdc3a15b7477ce93781a12018c17dde93df73b74
Josh Remaley <joshremaley@gmail.com>
2025-03-20 12:19:12 -0400

    Replace Firebase authorization with combined Firebase or Nostr authorization in takedown and get_artists_by_user_id routes

--- routes/admin.ts
+++ routes/admin.ts
@@ -1,20 +1,20 @@
 import express from "express";
-import { isFirebaseAuthorized } from "../middlewares/auth";
 import { isAdmin } from "../middlewares/isAdmin";
 import adminController from "../controllers/admin";
+import { isFirebaseOrNostrAuthorized } from "../middlewares/firebaseOrNostrAuth";
 
 const router = express.Router();
 
 router.post(
   "/takedown",
-  isFirebaseAuthorized,
+  isFirebaseOrNostrAuthorized,
   isAdmin,
   adminController.takedownContent
 );
 
 router.get(
   "/user/artists/:userId",
-  isFirebaseAuthorized,
+  isFirebaseOrNostrAuthorized,
   isAdmin,
   adminController.get_artists_by_user_id
 );
